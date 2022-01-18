a3f979fd9be1c5f5845cb69df3f3f153c40f99db
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2022-01-18 17:16:38 -0300

    React router dom funcionando

---
+++ src/pages/FormStep2/index.tsx
@@ -0,0 +1,10 @@
+import * as C from './styles';
+
+export const FormStep2 = () => {
+  return(
+    <C.Container>
+      Passo2
+    </C.Container>
+  )
+
+};
---
+++ src/pages/FormStep3/index.tsx
@@ -0,0 +1,10 @@
+import * as C from './styles';
+
+export const FormStep3 = () => {
+  return(
+    <C.Container>
+      Passo3
+    </C.Container>
+  )
+
+};
