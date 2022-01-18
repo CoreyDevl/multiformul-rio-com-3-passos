bc9d06404feff37a7eb5b629c02023044850340e
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2022-01-18 18:56:42 -0300

    ciraçao de componente tema  -
    componente que envolve em tema  e tambem o header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import * as C from './styles';
+
+export const Header = () => {
+  return (
+    <C.Container>
+      <h1>Cadastro de Desenvolvedor</h1>
+      <p>Cadastr-se e receba um emprego na hora!</p>
+    </C.Container>
+  )
+}
